c0b86000d5b15e83820048d76952e6684f41aa6f
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-04-15 20:59:47 -0600

    Update 6.sql

--- 6.sql
+++ 6.sql
@@ -1,4 +1,10 @@
+/*  Content Recommendations
 
+writing a
+query to return page recommendations
+to a social media user based on the
+pages that their friends have liked, but
+that they have not yet marked as liked. */
 
 create table friends_p6 (user_id integer, friend integer);
 
@@ -37,3 +43,18 @@ create table Flikes_p6 as
            on f.friend = l.user_id;
 
 select * from Flikes_p6;
+
+create table equals_p6 as
+select a.user_id, a.friend, a.friend_likes, 
+       l.page_likes as user_likes
+       from Flikes_p6 a
+       left join likes_p6 l
+       on a.user_id = l.user_id and a.friend_likes = l.page_likes
+       order by 1, 2;
+
+select * from equals_p6;
+
+select distinct user_id, friend_likes 
+       from equals_p6 
+       where user_likes is null
+       order by 1;
