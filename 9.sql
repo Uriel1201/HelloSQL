f63eebe3cb93d470be4fd05f89e3e5f0bacbfaa3
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-04-18 21:19:33 -0600

    Update 9.sql

--- 9.sql
+++ 9.sql
@@ -19,14 +19,14 @@ select * from friends_p9;
 
 create table numbers_p9 as
 select user_1 from friends_p9
-union all
+              union all
 select user_2 from friends_p9
-order by 1;
+              order by 1;
 
 select * from numbers_p9;
 
 select user_1 as user_id,
-    count(*) as number_of_friends
-    from numbers_p9
-    group by user_1
-    order by 2 desc, user_1; 
+       count(*) as number_of_friends
+                from numbers_p9
+                group by user_1
+                order by 2 desc, user_1; 
