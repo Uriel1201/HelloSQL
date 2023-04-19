8642b98bad0aae764d6c6b1c5849aeec104c7539
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-04-18 20:41:20 -0600

    Create 9.sql

---
+++ 9.sql
@@ -0,0 +1,32 @@
+/* 9. Most Friended.
+
+Returning a list of
+users and their corresponding friend
+count. Assuming that only
+unique friendships are displayed. */
+
+create table friends_p9 (user_1 integer, user_2 integer
+    );
+insert into friends_p9 with names as  (
+    select 1, 2 from dual union all 
+    select 1, 3 from dual union all 
+    select 1, 4 from dual union all 
+    select 2, 3 from dual
+)
+select * from names;
+
+select * from friends_p9;
+
+create table numbers_p9 as
+select user_1 from friends_p9
+union all
+select user_2 from friends_p9
+order by 1;
+
+select * from numbers_p9;
+
+select user_1 as user_id,
+    count(*) as number_of_friends
+    from numbers_p9
+    group by user_1
+    order by 2 desc, user_1; 
