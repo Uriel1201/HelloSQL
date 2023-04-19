9a82aacaa1e2134f7eb00d86b9c46cbb91ced7d9
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-04-18 22:29:46 -0600

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -5,9 +5,9 @@ time elapsed between the last action
 and the second-to-last action, in
 ascending order by user ID. */ 
 
-create table users (id integer, actions varchar (10), action_date date);
+create table users_p4 (id integer, actions varchar (10), action_date date);
 
-insert into users with names as (
+insert into users_p4 with names as (
     select 1, 'Start', '13-feb-20' from dual union all 
     select 1, 'Cancel', '13-feb-20' from dual union all 
     select 2, 'Start', '11-feb-20' from dual union all 
@@ -15,33 +15,34 @@ insert into users with names as (
     select 3, 'Start', '15-feb-20' from dual union all 
     select 3, 'Cancel', '15-feb-20' from dual union all 
     select 4, 'Start', '18-feb-20' from dual union all 
-    select 1, 'Publish', '19-feb-20' from dual)
-    select * from names;
+    select 1, 'Publish', '19-feb-20' from dual
+) select * from names;
 
 select * from users;
 
-create table numbers as
+create table numbers_p4 as
 select id, action_date, 
-       row_number() over (partition by id order by action_date desc) lasts
-       from users;
+       row_number() over (partition by id order by action_date desc) as
+       rank_action
+       from users_p4;
 
-select * from numbers;
+select * from numbers_p4;
 
-create table number1 as
-select id, action_date
-       from numbers 
-       where lasts = 1;
+create table n1_p4 as
+select id, action_date as last_action
+       from numbers_p4
+       where rank_action = 1;
 
-create table number2 as
-select id, action_date 
-       from numbers  
-       where lasts = 2;
+create table n2_p4 as
+select id, action_date as second_last_action
+       from numbers_p4  
+       where rank_action = 2;
 
-select * from number1;
-select * from number2;
+select * from n1_p4;
+select * from n2_p4;
 
-select a.id, (a.action_date - b.action_date) days_elapsed
-       from number1 a
-       left join number2 b
+select a.id, (a.last_action - b.second_last_action) as elapsed_days 
+       from n1_p4 a
+       left join n2_p4 b
        on a.id = b.id
        order by a.id;
