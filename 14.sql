c3e114d46d8dc10cd7e622ec3b2fc78a3e676d1b
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-06-15 17:09:26 -0600

    Update 14.sql

--- 14.sql
+++ 14.sql
@@ -1,3 +1,9 @@
+/* 14. Cumulative Salary.
+Writing a query to get, for each month, the
+cumulative sum of an employee’s salary
+over a period of 3 months, excluding the
+most recent month. */ 
+
 create table employee_p14 (id integer, pay_month integer, salary integer);
 
 insert into employee_p14 with names as (
