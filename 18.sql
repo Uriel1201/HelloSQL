c7a6ba690c4d27c13797d3399d42a7a9ddeb06da
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-06-17 16:04:06 -0600

    Update 18.sql

--- 18.sql
+++ 18.sql
@@ -1,3 +1,20 @@
+/* The same table of problem 17 */
+create table stations_p17 (id integer, city varchar (20), states varchar (20), latitude number, longitude number);
+
+insert into stations_p17 with names as (
+    select 1, 'Asheville',      'North Carolina', 35.6, 82.6  from dual union all 
+    select 2, 'Burlington',     'North Carolina', 36.1, 79.4  from dual union all
+    select 3, 'Chapel Hill',    'North Carolina', 35.9, 79.1  from dual union all
+    select 4, 'Davidson',       'North Carolina', 35.5, 80.8  from dual union all
+    select 5, 'Elizabeth City', 'North Carolina', 36.3, 76.3  from dual union all
+    select 6, 'Fargo',          'North Dakota',   46.9, 96.8  from dual union all
+    select 7, 'Grand Forks',    'North Dakota',   47.9, 97.0  from dual union all
+    select 8, 'Hettinger',      'North Dakota',   46.0, 102.6 from dual union all
+    select 9, 'Inkster',        'North Dakota',   48.2, 97.6  from dual
+) select * from names;
+
+select * from stations_p17;
+
 create table distances_p18 as
 select a.states, a.city as city1, b.city as city2, round (sqrt (power (abs (a.latitude - b.latitude),2) + 
     power (abs (a.longitude - b.longitude),2)),2) as distances
