06b00c4bba6bee441d7c52a831eef267b237b8d6
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-11 16:57:34 -0600

    Update Template.py

--- Template.py
+++ Template.py
@@ -1,1 +1,17 @@
 
+# pip install pandas
+# pip install SQLAlchemy
+# pip install cx_Oracle
+
+import pandas as pd 
+import cx_Oracle
+import sqlalchemy
+from sqlalchemy.exc import SQLAlchemyError
+try:
+  engine = sqlalchemy.create_engine("oracle+cx_oracle://usr:pswd@localhost/?service_name=orclpdb1", arraysize=1000)
+
+  tablename_sql = """select * from tablename""";
+  df_tablename = pd.read_sql(tablename_sql, engine)
+  print(df_tablename)
+except SQLAlchemyError as e:
+  print(e)
