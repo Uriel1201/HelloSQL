84af457e58e8fb8b8fd4e5848a8bbbaba3ab7ae4
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-13 12:48:31 -0600

    Update 01.sql

--- 01.sql
+++ 01.sql
@@ -24,11 +24,11 @@ create table totals_p1 as
 select user_id, sum(case when action='start' then 1 else 0 end) as starts,
 sum(case when action='cancel' then 1 else 0 end) as cancels,
 sum(case when action='publish' then 1 else 0 end) as publishes
-from users
+from users_p1
 group by user_id
 order by user_id;
 
-select * from totals_p1
+select * from totals_p1;
 
 select user_id, 1.0*publishes/starts as publish_rate, 1.0*cancels/starts as cancel_rate
 from totals_p1;
