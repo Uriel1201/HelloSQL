e64dfa18639b7d205cdd77f7f006030cc8d4af13
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-13 11:51:06 -0600

    Update 01.sql

--- 01.sql
+++ 01.sql
@@ -3,10 +3,10 @@
 Writing a query to return the publication and cancellation 
 rate for each user*/
 
-create table users (user_id integer, action varchar(9), dates
+create table users_p1 (user_id integer, action varchar(9), dates
 date);
 
-insert into users with
+insert into users_p1 with
 names as (select 1,'start','01-jan-20' from dual union all
 select 1,'cancel','02-jan-20' from dual union all
 select 2,'start','03-jan-20' from dual union all
