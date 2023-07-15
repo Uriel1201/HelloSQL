c86aab412498935b8065b5e35af30e5b9fffbd3f
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-15 13:38:27 -0600

    Update 03.sql

--- 03.sql
+++ 03.sql
@@ -4,21 +4,7 @@
 writing a query to return the most frequent item
 ordered on each date. */
 
-create table items_p3 (dates date, item varchar (9));
-
-insert into items_p3 with names as ( 
-    select '01-jan-20', 'apple'  from dual union all  
-    select '01-jan-20', 'apple'  from dual union all  
-    select '01-jan-20', 'pear'   from dual union all  
-    select '01-jan-20', 'pear'   from dual union all  
-    select '02-jan-20', 'pear'   from dual union all  
-    select '02-jan-20', 'pear'   from dual union all  
-    select '02-jan-20', 'pear'   from dual union all 
-    select '02-jan-20', 'orange' from dual
-    ) select * from names;
-
 select *  from items_p3;
-
 
 create table counts_p3 as 
        select dates, item, count(*) as counts 
