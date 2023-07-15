419dcb605c524f704f4c42cdb499ed4c1e30780b
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-15 13:34:31 -0600

    Update 03.sql

--- 03.sql
+++ 03.sql
@@ -1,21 +1,21 @@
-/*Most Frequent Item
+/*
+3. Most Frequent Item
 
 writing a query to return the most frequent item
-ordered on each date.*/
+ordered on each date. */
 
-create table items  (dates date, item varchar (9));
+create table items_p3 (dates date, item varchar (9));
 
-insert into items with  
-names as ( 
-    select '01-jan-20', 'apple' from dual union all  
-    select '01-jan-20', 'apple' from dual union all  
-    select '01-jan-20', 'pear' from dual union all  
-    select '01-jan-20', 'pear' from dual union all  
-    select '02-jan-20', 'pear' from dual union all  
-    select '02-jan-20', 'pear' from dual union all  
-    select '02-jan-20', 'pear' from dual union all 
-    select '02-jan-20', 'orange' from dual)  
-    select * from names;
+insert into items_p3 with names as ( 
+    select '01-jan-20', 'apple'  from dual union all  
+    select '01-jan-20', 'apple'  from dual union all  
+    select '01-jan-20', 'pear'   from dual union all  
+    select '01-jan-20', 'pear'   from dual union all  
+    select '02-jan-20', 'pear'   from dual union all  
+    select '02-jan-20', 'pear'   from dual union all  
+    select '02-jan-20', 'pear'   from dual union all 
+    select '02-jan-20', 'orange' from dual
+    ) select * from names;
 
 select *  from items;
 
