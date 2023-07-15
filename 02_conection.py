c5d31302a0dd1461ba350e47c3a25aaa9ef1729c
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-14 21:46:44 -0600

    Update 02_conection.py

--- 02_conection.py
+++ 02_conection.py
@@ -1,1 +1,20 @@
+# pip install pandas
+# pip install numpy
+# pip install SQLAlchemy
+# pip install cx_Oracle
 
+import pandas as pd 
+import numpy  as np
+import cx_Oracle
+import sqlalchemy
+
+from sqlalchemy.exc import SQLAlchemyError
+
+try:
+  engine = sqlalchemy.create_engine("oracle+cx_oracle://usr:pswd@localhost/?service_name=orclpdb1", arraysize=1000)
+
+  table = """select * from transactions_p2""";
+  transactions = pd.read_sql(table, engine)
+  transactions
+except SQLAlchemyError as e:
+  print(e)
