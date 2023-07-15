e8d3f8c1b53569598df752cd9a1ba605c535071e
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-15 16:37:11 -0600

    Update 03c_pandas.py

--- 03c_pandas.py
+++ 03c_pandas.py
@@ -9,12 +9,25 @@ import cx_Oracle
 import sqlalchemy
 from sqlalchemy.exc import SQLAlchemyError
 
+'''
+3. Most Frequent Item
+
+writing a query to return the most frequent item
+ordered on each date.
+'''
+
 try:
   engine = sqlalchemy.create_engine("oracle+cx_oracle://usr:pswd@localhost/?service_name=orclpdb1", arraysize=1000)
-
-  table = """select * from items_p3""";
-  items = pd.read_sql(table, engine)
+  data = """select * from items_p3""";
+  items = pd.read_sql(data, engine)
   items
   
 except SQLAlchemyError as e:
   print(e)
+
+item_counts = items.groupby(['dates','item']).agg({'item':np.size})
+item_counts.rename(columns = {'item':'count'}, inplace = True)
+item_counts.reset_index(inplace = True)
+item_counts['ranks'] = item_counts.group.by('dates')['count'].rank(method = 'min', ascending = False)
+
+item_counts[item_counts.ranks == 1.0][['dates','item']]
