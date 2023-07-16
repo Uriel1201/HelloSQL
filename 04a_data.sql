bac549d4e59f0a35ab46b3f4025a561d462d640c
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-16 00:37:10 -0600

    Update 04a_data.sql

--- 04a_data.sql
+++ 04a_data.sql
@@ -9,12 +9,12 @@ ascending order by user ID. */
 create table users_p4 (id integer, actions varchar (10), action_date date);
 
 insert into users_p4 with names as (
-    select 1, 'Start', '13-feb-20' from dual union all 
-    select 1, 'Cancel', '13-feb-20' from dual union all 
-    select 2, 'Start', '11-feb-20' from dual union all 
+    select 1, 'Start',   '13-feb-20' from dual union all 
+    select 1, 'Cancel',  '13-feb-20' from dual union all 
+    select 2, 'Start',   '11-feb-20' from dual union all 
     select 2, 'Publish', '14-feb-20' from dual union all 
-    select 3, 'Start', '15-feb-20' from dual union all 
-    select 3, 'Cancel', '15-feb-20' from dual union all 
-    select 4, 'Start', '18-feb-20' from dual union all 
+    select 3, 'Start',   '15-feb-20' from dual union all 
+    select 3, 'Cancel',  '15-feb-20' from dual union all 
+    select 4, 'Start',   '18-feb-20' from dual union all 
     select 1, 'Publish', '19-feb-20' from dual
 ) select * from names;
