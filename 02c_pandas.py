0de55a3932a3171e98ae3cf1807ec7d343f29e87
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-15 22:37:35 -0600

    Update 02c_pandas.py

--- 02c_pandas.py
+++ 02c_pandas.py
@@ -10,7 +10,7 @@ import sqlalchemy
 from sqlalchemy.exc import SQLAlchemyError
 
 '''
-1. Changes in Net Worth
+2. Changes in Net Worth
 
 Writing a query to return the change in net worth for each user, 
 ordered by decreasing net change.
