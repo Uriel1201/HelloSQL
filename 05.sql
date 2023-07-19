82a373a372f83b48d8e240a1598e1fb9cd1a12cc
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-18 19:41:29 -0600

    Update 05.sql

--- 05.sql
+++ 05.sql
@@ -9,20 +9,6 @@ date when they become a super user, ordered by oldest super users first.
 Users who are not super users should
 also be present in the table. */
 
-
-create table users_p5 (user_id integer, product_id integer, transaction_date date);
-
-insert into users_p5 with names as (
-    select 1, 101, '12-feb-20' from dual union all 
-    select 2, 105, '13-feb-20' from dual union all 
-    select 1, 111, '14-feb-20' from dual union all 
-    select 3, 121, '15-feb-20' from dual union all 
-    select 1, 101, '16-feb-20' from dual union all 
-    select 2, 105, '17-feb-20' from dual union all 
-    select 4, 101, '16-feb-20' from dual union all 
-    select 3, 105, '15-feb-20' from dual)
-    select * from names;
-
 select * from users_p5;
 
 create table ranks_p5 as
