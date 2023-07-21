635f63c825812e7e11f965f483ab1f1517da5b68
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-21 12:26:57 -0600

    Update 06c_pandas.py

--- 06c_pandas.py
+++ 06c_pandas.py
@@ -1,3 +1,34 @@
+# pip install pandas
+# pip install numpy
+# pip install SQLAlchemy
+# pip install cx_Oracle
+
+import pandas as pd 
+import numpy  as np
+import cx_Oracle
+import sqlalchemy
+from sqlalchemy.exc import SQLAlchemyError
+
+'''
+06. Content Recommendations
+
+Writing a query to return page recommendations
+to a social media user based on the
+pages that their friends have liked, but
+that they have not yet marked as liked.
+'''
+
+try:
+  engine = sqlalchemy.create_engine("oracle+cx_oracle://usr:pswd@localhost/?service_name=orclpdb1", arraysize=1000)
+  data1 = """select * from friends_p6""";
+  data2 = """select * from likes_p6""";
+
+  friends = pd.read_sql(data1, engine)
+  likes   = pd.read_sql(data2, engine)
+  
+except SQLAlchemyError as e:
+  print(e)
+
 likes['page_friend_likes'] = likes['page_likes']
 
 i = ['user_id','page_friend_likes']
