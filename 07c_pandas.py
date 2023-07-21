80d6e5dd010a9a501cacfe3e2152863af77f3ff7
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-21 15:20:15 -0600

    Update 07c_pandas.py

--- 07c_pandas.py
+++ 07c_pandas.py
@@ -1,3 +1,34 @@
+# pip install pandas
+# pip install numpy
+# pip install SQLAlchemy
+# pip install cx_Oracle
+
+import pandas as pd 
+import numpy  as np
+import cx_Oracle
+import sqlalchemy
+from sqlalchemy.exc import SQLAlchemyError
+
+'''
+07. Mobile and Web Visitors.
+
+Returning the
+fraction of users who only visited
+mobile, only visited web, and visited
+both. 
+'''
+
+try:
+  engine = sqlalchemy.create_engine("oracle+cx_oracle://usr:pswd@localhost/?service_name=orclpdb1", arraysize=1000)
+  data1 = """select * from mobile_p7""";
+  data2 = """select * from web_p7""";
+
+  mobile = pd.read_sql(data1, engine)
+  web    = pd.read_sql(data2, engine)
+  
+except SQLAlchemyError as e:
+  print(e)
+
 index = 'user_id'
 mobile_users = pd.DataFrame(mobile[index].copy())
 mobile_users['mobile_id'] = mobile[index]
