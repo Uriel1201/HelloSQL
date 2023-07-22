2d9ad12108ba155eb178e3684e04220d383f6ad5
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-22 12:26:45 -0600

    Update 09b_sql.sql

--- 09b_sql.sql
+++ 09b_sql.sql
@@ -6,15 +6,6 @@ users and their corresponding friend
 count. Assuming that only
 unique friendships are displayed. */
 
-create table friends_p9 (user_1 integer, user_2 integer);
-
-insert into friends_p9 with names as (
-    select 1, 2 from dual union all 
-    select 1, 3 from dual union all 
-    select 1, 4 from dual union all 
-    select 2, 3 from dual
-) select * from names;
-
 select * from friends_p9;
 
 create table numbers_p9 as
