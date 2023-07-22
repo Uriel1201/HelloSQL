6de4cba49c3735b312e88518445d0337cf0cfea0
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-22 14:11:28 -0600

    Update 10.sql

--- 10.sql
+++ 10.sql
@@ -1,4 +1,5 @@
-/* 10. Project Aggregation 
+/* 
+10. Project Aggregation 
 
 Writing a query to return the start and end
 dates of each project, and the number of
@@ -6,7 +7,7 @@ days it took to complete. */
 
 create table projects_p10 (task_id integer, start_date date, end_date date);
 
-insert into projects_p10 with names as  (
+insert into projects_p10 with names as (
     select 1, '01-Oct-20', '02-Oct-20' from dual union all
     select 2, '02-Oct-20', '03-Oct-20' from dual union all
     select 3, '03-Oct-20', '04-Oct-20' from dual union all
@@ -14,8 +15,7 @@ insert into projects_p10 with names as  (
     select 5, '14-Oct-20', '15-Oct-20' from dual union all
     select 6, '28-Oct-20', '29-Oct-20' from dual union all
     select 7, '30-Oct-20', '31-Oct-20' from dual
-)
-select * from names;
+) select * from names;
 
 select * from projects_p10;
 
