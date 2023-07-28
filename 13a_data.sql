3317d56439821f0ae3c99e4cba82e1ed324ed630
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-28 11:59:58 -0600

    Update 13a_data.sql

--- 13a_data.sql
+++ 13a_data.sql
@@ -1,1 +1,17 @@
+/*
+13. Rank Without Rank.
 
+Writing a query to rank scores in the
+following table without using a window
+function. */ 
+
+create table scores_p13 (id integer, score number);
+
+insert into scores_p13 with names as (
+    select 1, 3.50 from dual union all 
+    select 2, 3.65 from dual union all 
+    select 3, 4.00 from dual union all 
+    select 4, 3.85 from dual union all 
+    select 5, 4.00 from dual union all 
+    select 6, 3.65 from dual
+) select * from names;
