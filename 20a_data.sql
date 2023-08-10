02ad06c1169d05bf5f2f58c864125fc9fbaf8fd0
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-08-09 20:59:52 -0600

    Update 20a_data.sql

--- 20a_data.sql
+++ 20a_data.sql
@@ -1,1 +1,14 @@
 
+*/
+create table attendance_p20 (event_date date, visitors integer);
+
+insert into attendance_p20 with names as ( 
+    select '01-JAN-20', 10   from dual union all
+    select '04-JAN-20', 109  from dual union all
+    select '05-JAN-20', 150  from dual union all
+    select '06-JAN-20', 99   from dual union all 
+    select '07-JAN-20', 145  from dual union all  
+    select '08-JAN-20', 1455 from dual union all 
+    select '11-JAN-20', 199  from dual union all 
+    select '12-JAN-20', 188  from dual
+) select * from names;
