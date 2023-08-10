8bc71a07a9800e52719de9dd6cb1d16863f4b0cd
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-08-09 20:51:29 -0600

    Update and rename 20.sql to 20b_sql.sql

--- 20b_sql.sql
+++ 20b_sql.sql
@@ -1,15 +1,13 @@
-create table attendance_p20 (event_date date, visitors integer);
+/*
+20. Three in a row
 
-insert into attendance_p20 with names as ( 
-    select '01-JAN-20', 10   from dual union all
-    select '04-JAN-20', 109  from dual union all
-    select '05-JAN-20', 150  from dual union all
-    select '06-JAN-20', 99   from dual union all 
-    select '07-JAN-20', 145  from dual union all  
-    select '08-JAN-20', 1455 from dual union all 
-    select '11-JAN-20', 199  from dual union all 
-    select '12-JAN-20', 188  from dual
-) select * from names;
+Writing a query to return a
+table showing the date and visitor count
+of high-attendance periods, defined as
+three consecutive entries (not
+necessarily consecutive dates) with
+more than 100 visitors.
+*/
 
 select * from attendance_p20;
 
