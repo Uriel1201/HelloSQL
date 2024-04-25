86945316e4a1690de2d2f0b934666f1bc20b7c3d
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-04-25 17:00:36 -0600

    Update 01_script.py

--- 01_script.py
+++ 01_script.py
@@ -1,31 +1,8 @@
-# %%
-# !! {"metadata":{
-# !!   "id": "pb2s4sGTfKUO"
-# !! }}
 """
 # 01. Cancelation Rates
 From the following table of user IDs, actions, and dates, write a query to return the publication and cancellation rate for each user.
 """
 
-# %%
-# !! {"metadata":{
-# !!   "id": "daiZQJANfK2_",
-# !!   "colab": {
-# !!     "base_uri": "https://localhost:8080/",
-# !!     "height": 209
-# !!   },
-# !!   "executionInfo": {
-# !!     "status": "ok",
-# !!     "timestamp": 1714084295653,
-# !!     "user_tz": 360,
-# !!     "elapsed": 244,
-# !!     "user": {
-# !!       "displayName": "Uriel Garc\u00eda",
-# !!       "userId": "03386744220426758265"
-# !!     }
-# !!   },
-# !!   "outputId": "62ebfad0-e371-4675-c2ca-3c82112d57de"
-# !! }}
 # pip install pandas
 # pip install numpy
 # pip install SQLAlchemy
@@ -38,12 +15,6 @@ import numpy  as np
 # from sqlalchemy.exc import SQLAlchemyError
 
 '''
-01. Cancellation Rates.
-
-Writing a query to return the publication and cancellation
-rate for each user
-
-
 try:
   engine = sqlalchemy.create_engine("oracle+cx_oracle://usr:pswd@localhost/?service_name=orclpdb1", arraysize=1000)
 
@@ -55,25 +26,6 @@ except SQLAlchemyError as e:
   print(e)
 '''
 
-# %%
-# !! {"metadata":{
-# !!   "colab": {
-# !!     "base_uri": "https://localhost:8080/",
-# !!     "height": 331
-# !!   },
-# !!   "id": "iQ7zcNQLf_iL",
-# !!   "executionInfo": {
-# !!     "status": "ok",
-# !!     "timestamp": 1714084305963,
-# !!     "user_tz": 360,
-# !!     "elapsed": 176,
-# !!     "user": {
-# !!       "displayName": "Uriel Garc\u00eda",
-# !!       "userId": "03386744220426758265"
-# !!     }
-# !!   },
-# !!   "outputId": "7b298d52-e7ef-49f1-84d9-3c8fd2b3bb31"
-# !! }}
 data = {'user_id' :[1,1,2,1,1,2,3,3,4],
         'action'  :['start','cancel','start',
                     'start','publish','publish',
@@ -90,90 +42,17 @@ data = {'user_id' :[1,1,2,1,1,2,3,3,4],
         }
 
 users = pd.DataFrame(data)
-users
+# users
 
-# %%
-# !! {"metadata":{
-# !!   "colab": {
-# !!     "base_uri": "https://localhost:8080/",
-# !!     "height": 664
-# !!   },
-# !!   "id": "Q0n86Eo3W2R_",
-# !!   "executionInfo": {
-# !!     "status": "ok",
-# !!     "timestamp": 1714084315994,
-# !!     "user_tz": 360,
-# !!     "elapsed": 366,
-# !!     "user": {
-# !!       "displayName": "Uriel Garc\u00eda",
-# !!       "userId": "03386744220426758265"
-# !!     }
-# !!   },
-# !!   "outputId": "70f386ef-0966-4138-fbd0-6a9248996703"
-# !! }}
 users['starts']    = np.where(users['action']=='start',  1,0)
 users['cancels']   = np.where(users['action']=='cancel', 1,0)
 users['publishes'] = np.where(users['action']=='publish',1,0)
-users
+# users
 
-# %%
-# !! {"metadata":{
-# !!   "colab": {
-# !!     "base_uri": "https://localhost:8080/",
-# !!     "height": 194
-# !!   },
-# !!   "id": "YMJZ32h9Yaem",
-# !!   "executionInfo": {
-# !!     "status": "ok",
-# !!     "timestamp": 1714084350479,
-# !!     "user_tz": 360,
-# !!     "elapsed": 249,
-# !!     "user": {
-# !!       "displayName": "Uriel Garc\u00eda",
-# !!       "userId": "03386744220426758265"
-# !!     }
-# !!   },
-# !!   "outputId": "842c6f7f-8af0-4c03-9ddf-8c52fb7d3df1"
-# !! }}
 actions = users[['user_id','starts','cancels','publishes']].groupby('user_id').sum()
 actions.reset_index(inplace = True)
-actions
+# actions
 
-# %%
-# !! {"metadata":{
-# !!   "colab": {
-# !!     "base_uri": "https://localhost:8080/",
-# !!     "height": 194
-# !!   },
-# !!   "id": "WEdvTOh2YipV",
-# !!   "executionInfo": {
-# !!     "status": "ok",
-# !!     "timestamp": 1714084949438,
-# !!     "user_tz": 360,
-# !!     "elapsed": 774,
-# !!     "user": {
-# !!       "displayName": "Uriel Garc\u00eda",
-# !!       "userId": "03386744220426758265"
-# !!     }
-# !!   },
-# !!   "outputId": "894b000f-4c7a-4e19-ffc0-ee80b60859da"
-# !! }}
 actions['cancel_rate']  = actions['cancels']   / actions['starts']
 actions['publish_rate'] = actions['publishes'] / actions['starts']
-
 actions[['user_id','cancel_rate','publish_rate']]
-
-# %%
-# !! {"main_metadata":{
-# !!   "colab": {
-# !!     "provenance": [],
-# !!     "authorship_tag": "ABX9TyPpNGhKzG4N2hbTlc9nhIE/"
-# !!   },
-# !!   "kernelspec": {
-# !!     "name": "python3",
-# !!     "display_name": "Python 3"
-# !!   },
-# !!   "language_info": {
-# !!     "name": "python"
-# !!   }
-# !! }}
