aefeb2e83f64f03809984bad5ad6dcdb9632e84a
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-04-27 21:41:25 -0600

    Update 04_script.py

--- 04_script.py
+++ 04_script.py
@@ -21,65 +21,28 @@ from sqlalchemy.exc import SQLAlchemyError
 try:
   engine = sqlalchemy.create_engine("oracle+cx_oracle://usr:pswd@localhost/?service_name=orclpdb1", arraysize=1000)
 
-  table = """select * from items_p3""";
-  items = pd.read_sql(table, engine)
+  table = """select * from users_p4""";
+  users = pd.read_sql(table, engine)
+  users
+  
+  users['action_date'] = pd.to_datetime(users['action_date'])
 
-import pandas as pd
-import numpy as  np
+  # Defining a function to count in descending order
+  def row_num(x):
+      x_s = pd.Series(x)
+      row_n = range(len(x), 0, -1)
+      return row_n
 
-data = {'user_id' : [1,1,2,2,3,3,4,1],
-        'actions'    : ['start','cancel','start',
-                    'publish','start','cancel',
-                    'start','publish'],
-        'action_date'   : ['12-FEB-20',
-                         '13-FEB-20',
-                         '11-FEB-20',
-                         '14-FEB-20',
-                         '15-FEB-20',
-                         '15-FEB-20',
-                         '18-FEB-20',
-                         '19-FEB-20']
-        }
+  users['date_rank'] = users.groupby('user_id')['action_date'].transform(row_num)
 
-users = pd.DataFrame(data)
-users
+  last_act = users[['user_id', 'action_date']][users['date_rank'] == 1]
+  last_act.rename(columns = {'action_date' : 'last'}, inplace = True)
 
-users['action_date'] = pd.to_datetime(users['action_date'])
+  slast_act = users[['user_id', 'action_date']][users['date_rank'] == 2]
+  slast_act.rename(columns = {'action_date' : 'penultimate'}, inplace = True)
 
-users
+  elapsed_days = pd.merge(last_act, slast_act, on = 'user_id', how = 'left').sort_values(by = 'user_id')
+  elapsed_days.reset_index(drop = True, inplace = True)
+  elapsed_days['elapsed_time'] = elapsed_days['last'] - elapsed_days['penultimate']
 
-def row_num(x):
-    x_s = pd.Series(x)
-    row_n = range(len(x), 0, -1)
-    return row_n
-
-users['date_rank'] = users.groupby('user_id')['action_date'].transform(row_num)
-
-users
-
-last_act = users[['user_id', 'action_date']][users['date_rank'] == 1]
-last_act.rename(columns = {'action_date' : 'last'}, inplace = True)
-
-last_act
-
-
-slast_act = users[['user_id', 'action_date']][users['date_rank'] == 2]
-slast_act.rename(columns = {'action_date' : 'penultimate'}, inplace = True)
-
-
-slast_act
-
-elapsed_days = pd.merge(last_act, slast_act, on = 'user_id', how = 'left').sort_values(by = 'user_id')
-elapsed_days.reset_index(drop = True, inplace = True)
-
-
-elapsed_days
-
-elapsed_days['elapsed_time'] = elapsed_days['last'] - elapsed_days['penultimate']
-
-# %%
-
-elapsed_days
-
-
-elapsed_days[['user_id', 'elapsed_time']].sort_values(by = 'user_id')
+  elapsed_days[['user_id', 'elapsed_time']].sort_values(by = 'user_id')
