1dea7882af150bf25e15227c47fea975c114d58b
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-05-04 12:10:23 -0600

    Update and rename 06_script.py to 06_Content_recommendation/06_script.py

--- 06_Content_recommendation/06_script.py
+++ 06_Content_recommendation/06_script.py
@@ -11,57 +11,23 @@ Order the result by ascending user ID.
 
 
 friends
-
-
 likes
 
-
 friends_likes = pd.merge(friends, likes, left_on = 'friend', right_on = 'user_id')
-
-
-friends_likes
-
-
 friends_likes.drop('user_id_y', axis = 1, inplace = True)
 friends_likes.rename(columns = {'user_id_x' : 'user_id', 'page_likes' : 'friend_page_likes'}, inplace = True)
-
-
-friends_likes
-
-
 friends_likes.set_index(['user_id', 'friend_page_likes'], inplace = True, drop = False)
-user_likes = likes.set_index(['user_id', 'page_likes'], drop = False)
-
-
 friends_likes.rename_axis(index = {'friend_page_likes' : 'page_likes'}, inplace = True)
 
-friends_likes
-
-
-user_likes
-
-
+user_likes = likes.set_index(['user_id', 'page_likes'], drop = False)
 user_likes.drop('user_id', axis = 1, inplace = True)
-
-
-user_likes
-
 
 is_liked = pd.concat([friends_likes, user_likes], axis = 1, join = 'outer')
 
-
-is_liked
-
-
 recomendations = is_liked[['user_id']][is_liked['page_likes'].isnull()]
-
-
 recomendations.drop('user_id', axis = 1,inplace = True)
 recomendations.reset_index(inplace = True)
-
-
 recomendations.rename(columns = {'page_likes' : 'recommended_page'}, inplace = True)
 recomendations.drop_duplicates(inplace = True)
-
 
 recomendations.sort_values(by = 'user_id')
