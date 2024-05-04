a25dd8aabf2e7f7b86a8e1f5eae7ded2123bea13
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-05-04 12:19:07 -0600

    Update 06_script.py

--- 06_Content_recommendation/06_script.py
+++ 06_Content_recommendation/06_script.py
@@ -21,27 +21,26 @@ from sqlalchemy.exc import SQLAlchemyError
 try:
   engine = sqlalchemy.create_engine("oracle+cx_oracle://usr:pswd@localhost/?service_name=orclpdb1", arraysize=1000)
 
-  table1 = """select * from friends_p6""";
+  table1 = """select * from friends_p6;"""
   friends = pd.read_sql(table1, engine)
+  table2 = """select * from likes_p6;"""
+  likes = pd.read_sql(table2, engine)
 
-friends
-likes
+  friends_likes = pd.merge(friends, likes, left_on = 'friend', right_on = 'user_id')
+  friends_likes.drop('user_id_y', axis = 1, inplace = True)
+  friends_likes.rename(columns = {'user_id_x' : 'user_id', 'page_likes' : 'friend_page_likes'}, inplace = True)
+  friends_likes.set_index(['user_id', 'friend_page_likes'], inplace = True, drop = False)
+  friends_likes.rename_axis(index = {'friend_page_likes' : 'page_likes'}, inplace = True)
 
-friends_likes = pd.merge(friends, likes, left_on = 'friend', right_on = 'user_id')
-friends_likes.drop('user_id_y', axis = 1, inplace = True)
-friends_likes.rename(columns = {'user_id_x' : 'user_id', 'page_likes' : 'friend_page_likes'}, inplace = True)
-friends_likes.set_index(['user_id', 'friend_page_likes'], inplace = True, drop = False)
-friends_likes.rename_axis(index = {'friend_page_likes' : 'page_likes'}, inplace = True)
+  user_likes = likes.set_index(['user_id', 'page_likes'], drop = False)
+  user_likes.drop('user_id', axis = 1, inplace = True)
 
-user_likes = likes.set_index(['user_id', 'page_likes'], drop = False)
-user_likes.drop('user_id', axis = 1, inplace = True)
+  is_liked = pd.concat([friends_likes, user_likes], axis = 1, join = 'outer')
 
-is_liked = pd.concat([friends_likes, user_likes], axis = 1, join = 'outer')
+  recomendations = is_liked[['user_id']][is_liked['page_likes'].isnull()]
+  recomendations.drop('user_id', axis = 1,inplace = True)
+  recomendations.reset_index(inplace = True)
+  recomendations.rename(columns = {'page_likes' : 'recommended_page'}, inplace = True)
+  recomendations.drop_duplicates(inplace = True)
 
-recomendations = is_liked[['user_id']][is_liked['page_likes'].isnull()]
-recomendations.drop('user_id', axis = 1,inplace = True)
-recomendations.reset_index(inplace = True)
-recomendations.rename(columns = {'page_likes' : 'recommended_page'}, inplace = True)
-recomendations.drop_duplicates(inplace = True)
-
-recomendations.sort_values(by = 'user_id')
+  recomendations.sort_values(by = 'user_id')
