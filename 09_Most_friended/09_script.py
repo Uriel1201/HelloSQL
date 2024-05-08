5c5b72c4ca1e3d86ddc93c401f830abbc368f484
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-05-07 21:08:08 -0600

    Update 09_script.py

--- 09_Most_friended/09_script.py
+++ 09_Most_friended/09_script.py
@@ -7,10 +7,26 @@ and in the case of a tie, by ascending user ID.
 Assume that only unique friendships are displayed
 """
 
-friend_counts = pd.DataFrame(pd.concat([friends['user_1'], friends['user_2']]))
-friend_counts = pd.DataFrame(friend_counts.groupby(0).size())
-friend_counts.rename(columns = {0 : 'number_friends'}, inplace = True)
-friend_counts.reset_index(inplace = True)
-friend_counts.rename(columns = {0 : 'user_id'}, inplace = True)
+# pip install SQLAlchemy
+# pip install cx_Oracle
 
-friend_counts
+import pandas as pd
+import numpy  as np
+import cx_Oracle
+import sqlalchemy
+from sqlalchemy.exc import SQLAlchemyError
+
+try:
+  engine = sqlalchemy.create_engine("oracle+cx_oracle://usr:pswd@localhost/?service_name=orclpdb1", arraysize=1000)
+
+  table = """select * from friends_p9;"""
+  friends= pd.read_sql(table, engine)
+  friends
+
+  friend_counts = pd.DataFrame(pd.concat([friends['user_1'], friends['user_2']]))
+  friend_counts = pd.DataFrame(friend_counts.groupby(0).size())
+  friend_counts.rename(columns = {0 : 'number_friends'}, inplace = True)
+  friend_counts.reset_index(inplace = True)
+  friend_counts.rename(columns = {0 : 'user_id'}, inplace = True)
+
+  friend_counts
