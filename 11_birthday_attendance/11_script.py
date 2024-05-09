f8c4947c1531f51d0c27e2559ad29549266b6416
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-05-08 18:07:29 -0600

    Update 11_script.py

--- 11_birthday_attendance/11_script.py
+++ 11_birthday_attendance/11_script.py
@@ -4,24 +4,43 @@ Given the following two tables, write a query to return the fraction of students
 rounded to two decimal places, who attended school  (attendance = 1) on their birthday.
 """
 
-attendance['school_date'] = pd.to_datetime(attendance['school_date'])
-students['date_birth'] = pd.to_datetime(students['date_birth'])
+# pip install pandas
+# pip install numpy
+# pip install SQLAlchemy
+# pip install cx_Oracle
 
-attendance['day'] = attendance['school_date'].dt.day
-attendance['month'] = attendance['school_date'].dt.month
-attendance = attendance[['student_id', 'day', 'month', 'attendance']]
+import pandas as pd
+import numpy  as np
+import cx_Oracle
+import sqlalchemy
+from sqlalchemy.exc import SQLAlchemyError
 
-students['day'] = students['date_birth'].dt.day
-students['month'] = students['date_birth'].dt.month
-student_births = students[['student_id', 'day', 'month']]
+try:
+  engine = sqlalchemy.create_engine("oracle+cx_oracle://usr:pswd@localhost/?service_name=orclpdb1", arraysize=1000)
 
-attendance.set_index(['student_id', 'day', 'month'], inplace = True, drop = True)
-student_births.set_index(['student_id', 'day', 'month'], inplace = True, drop = True)
+  table1 = """select * from attendance_p11;"""
+  attendance = pd.read_sql(table1, engine)
+  table2 = """select * from students_p11;"""
+  students = pd.read_sql(table2, engine)
 
-birthday_attendance = pd.concat([attendance, student_births], axis = 1, join = 'inner')
+  attendance['school_date'] = pd.to_datetime(attendance['school_date'])
+  students['date_birth'] = pd.to_datetime(students['date_birth'])
 
-i = np.where(birthday_attendance['attendance'] == 'T', 1.0, 0.0).sum() / len(birthday_attendance)
-d = {'rate' : [i]}
-birthday_attendance_rate = pd.DataFrame(d)
+  attendance['day'] = attendance['school_date'].dt.day
+  attendance['month'] = attendance['school_date'].dt.month
+  attendance = attendance[['student_id', 'day', 'month', 'attendance']]
 
-birthday_attendance_rate.round(2)
+  students['day'] = students['date_birth'].dt.day
+  students['month'] = students['date_birth'].dt.month
+  student_births = students[['student_id', 'day', 'month']]
+
+  attendance.set_index(['student_id', 'day', 'month'], inplace = True, drop = True)
+  student_births.set_index(['student_id', 'day', 'month'], inplace = True, drop = True)
+
+  birthday_attendance = pd.concat([attendance, student_births], axis = 1, join = 'inner')
+
+  i = np.where(birthday_attendance['attendance'] == 'T', 1.0, 0.0).sum() / len(birthday_attendance)
+  d = {'rate' : [i]}
+  birthday_attendance_rate = pd.DataFrame(d)
+
+  birthday_attendance_rate.round(2)
