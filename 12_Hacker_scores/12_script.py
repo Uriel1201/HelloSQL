d1fa64a948739a14dce38c9a9650da9c44bccdeb
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-05-10 13:14:23 -0600

    Update 12_script.py

--- 12_Hacker_scores/12_script.py
+++ 12_Hacker_scores/12_script.py
@@ -12,34 +12,39 @@ Do not display entries for hackers with a score of zero.
 sub_p_res = subprocess.run(['wget', 'https://raw.githubusercontent.com/Uriel1201/HelloSQL/main/function_df.py'], stdout=subprocess.PIPE).stdout.decode('utf-8') #<cc-cm>
 print(sub_p_res) #<cc-cm>
 
+# pip install pandas
+# pip install numpy
+# pip install SQLAlchemy
+# pip install cx_Oracle
+
 import pandas as pd
 import numpy  as np
-import function_df as f_df
+import cx_Oracle
+import sqlalchemy
+from sqlalchemy.exc import SQLAlchemyError
 
-data1 = {'hacker_id' : [1,2,3,4],
-         'name'      : ['John','Jane','Joe','Jim']
-        }
+try:
+  engine = sqlalchemy.create_engine("oracle+cx_oracle://usr:pswd@localhost/?service_name=orclpdb1", arraysize=1000)
 
-data2 = {'sub_id'       : [101,102,103,104,105,106,107,108,109],
-         'hacker_id'    : [1,1,2,2,2,3,3,3,4],
-         'challenge_id' : [1,1,1,1,2,1,2,2,1],
-         'score'        : [10,12,11,9,13,9,12,15,0]
-        }
+  table1 = """select * from mobile_p7;"""
+  mobile = pd.read_sql(table1, engine)
+  mobile
 
-hackers     = pd.DataFrame(data1)
-submissions = pd.DataFrame(data2)
+  table2 = """select * from web_p7;"""
+  web = pd.read_sql(table2, engine)
+  web
 
-submissions = submissions.sort_values(by = ['hacker_id', 'challenge_id', 'score'])
-submissions['score_rank'] = submissions.groupby(['hacker_id', 'challenge_id'])['score'].transform(f_df.desc_row_num)
+  submissions = submissions.sort_values(by = ['hacker_id', 'challenge_id', 'score'])
+  submissions['score_rank'] = submissions.groupby(['hacker_id', 'challenge_id'])['score'].transform(f_df.desc_row_num)
 
-c = submissions['score_rank'] == 1
-max_scores = submissions.loc[c, ['hacker_id', 'score']]
-max_scores.set_index('hacker_id', inplace = True, drop = True)
+  c = submissions['score_rank'] == 1
+  max_scores = submissions.loc[c, ['hacker_id', 'score']]
+  max_scores.set_index('hacker_id', inplace = True, drop = True)
 
-total_score = max_scores.groupby(level=0).sum()
-total_score = total_score[total_score['score'] > 0]
+  total_score = max_scores.groupby(level=0).sum()
+  total_score = total_score[total_score['score'] > 0]
 
-hackers.set_index('hacker_id', inplace = True, drop = True)
+  hackers.set_index('hacker_id', inplace = True, drop = True)
 
-hacker_scores = pd.concat([hackers, total_score], axis = 1, join = 'inner')
-hacker_scores = hacker_scores.sort_values(by = 'score', ascending = False)
+  hacker_scores = pd.concat([hackers, total_score], axis = 1, join = 'inner')
+  hacker_scores = hacker_scores.sort_values(by = 'score', ascending = False)
