616d5ac35b33ccaa3dd47832b37a43a06e51d517
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-05-10 12:41:10 -0600

    Update 12_script.py

--- 12_Hacker_scores/12_script.py
+++ 12_Hacker_scores/12_script.py
@@ -11,7 +11,7 @@ Do not display entries for hackers with a score of zero.
 
 sub_p_res = subprocess.run(['wget', 'https://raw.githubusercontent.com/Uriel1201/HelloSQL/main/function_df.py'], stdout=subprocess.PIPE).stdout.decode('utf-8') #<cc-cm>
 print(sub_p_res) #<cc-cm>
-
+'''
 import pandas as pd
 import numpy  as np
 import function_df as f_df
@@ -43,3 +43,4 @@ hackers.set_index('hacker_id', inplace = True, drop = True)
 
 hacker_scores = pd.concat([hackers, total_score], axis = 1, join = 'inner')
 hacker_scores.sort_values(by = 'score', ascending = False)
+'''
